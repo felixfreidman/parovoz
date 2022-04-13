ad2af32b2294099c81525e4133d1269a1e0bd19d
Felix <felixfreidman@gmail.com>
2022-04-13 20:35:37 +0800

    8:35PM Commit Сделал страницу контактов

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -18,6 +18,27 @@ if (document.querySelector(".article-card")) {
 
 if (document.getElementById("orderDate")) {
   document.getElementById('orderDate').valueAsDate = new Date();
+}
+
+if (document.getElementById("map")) {
+  var init = function init() {
+    var myMap = new ymaps.Map('map', {
+      center: [56.81122355, 60.72763708],
+      zoom: 14
+    }),
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+      balloonContent: 'Летняя, 20',
+      iconCaption: 'Летняя, 20'
+    }, {
+      iconLayout: 'default#image',
+      iconImageHref: './assets/images/content/logo.svg',
+      iconImageSize: [30, 42],
+      iconImageOffset: [-5, -38]
+    });
+    myMap.geoObjects.add(myPlacemark);
+  };
+
+  ymaps.ready(init);
 } // // header-swiper
 // var swiper = new Swiper('#main-swiper', {
 //   fadeEffect: {
--- src/assets/js/components/common.js
+++ src/assets/js/components/common.js
@@ -21,5 +21,29 @@ if (document.querySelector(".article-card")) {
 }
 if (document.getElementById("orderDate")) {
 
-document.getElementById('orderDate').valueAsDate = new Date();
+  document.getElementById('orderDate').valueAsDate = new Date();
+}
+if (document.getElementById("map")) {
+  ymaps.ready(init);
+
+  function init() {
+    var myMap = new ymaps.Map('map', {
+      center: [56.81122355, 60.72763708],
+      zoom: 14
+    }),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        balloonContent: 'Летняя, 20',
+        iconCaption: 'Летняя, 20',
+      }, {
+        iconLayout: 'default#image',
+        iconImageHref: './assets/images/content/logo.svg',
+        iconImageSize: [30, 42],
+        iconImageOffset: [-5, -38],
+      })
+
+    myMap.geoObjects
+      .add(myPlacemark);
+  }
+
 }
