96f698b6fa43190bc225587e4562656238e6c5ce
Felix <felixfreidman@gmail.com>
2022-04-19 18:17:03 +0800

    6:16PM Commit отзывы и фильтрация все

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -239,13 +239,56 @@ $("#slider").slider({
   max: 5,
   min: 1,
   step: 1,
-  value: 5,
   range: true,
   values: [1, 5],
   slide: function slide(event, ui) {
     $("#starRating").val(ui.value);
+    $(".controls-section__button").removeClass("js--hidden");
+    filterFeedbacksByRating();
   }
 });
+
+if (document.getElementById("ratingForm")) {
+  var form = document.getElementById("ratingForm");
+  var select = document.getElementById("bathType");
+  form.addEventListener("reset", function () {
+    $("#slider").slider("values", [1, 5]);
+    $(".controls-section__button").addClass("js--hidden");
+    var allFeedbacks = document.querySelectorAll('.feedback-item');
+    allFeedbacks.forEach(function (feedback) {
+      feedback.classList.remove("js--hidden");
+    });
+  });
+  select.addEventListener("change", function () {
+    $(".controls-section__button").removeClass("js--hidden");
+    var checkedOption = $(select).val();
+    filterFeedbacksByName(checkedOption);
+  });
+}
+
+function filterFeedbacksByRating() {
+  var allFeedbacks = document.querySelectorAll('.feedback-item');
+  var starRating = $("#slider").slider("values");
+  allFeedbacks.forEach(function (feedback) {
+    if (feedback.querySelector(".amount").textContent != $("#starRating").val()) {
+      feedback.classList.add("js--hidden");
+    } else {
+      feedback.classList.remove("js--hidden");
+    }
+  });
+}
+
+function filterFeedbacksByName(checkedValue) {
+  var allFeedbacks = document.querySelectorAll('.feedback-item');
+  allFeedbacks.forEach(function (feedback) {
+    if (feedback.querySelector(".feedback-item__caption").textContent != checkedValue) {
+      feedback.classList.add("js--hidden");
+    } else {
+      feedback.classList.remove("js--hidden");
+    }
+  });
+}
+
 var swiper = new Swiper('#workerSwiper', {
   // Optional parameters
   direction: 'horizontal',
--- src/assets/js/components/feedback.js
+++ src/assets/js/components/feedback.js
@@ -3,10 +3,57 @@ $("#slider").slider({
     max: 5,
     min: 1,
     step: 1,
-    value: 5,
     range: true,
     values: [1, 5],
     slide: function (event, ui) {
         $("#starRating").val(ui.value);
+        $(".controls-section__button").removeClass("js--hidden");
+        filterFeedbacksByRating()
     }
 });
+
+if (document.getElementById("ratingForm")) {
+    const form = document.getElementById("ratingForm");
+    const select = document.getElementById("bathType");
+    form.addEventListener("reset", () => {
+        $("#slider").slider("values", [1, 5])
+        $(".controls-section__button").addClass("js--hidden");
+
+        var allFeedbacks = document.querySelectorAll('.feedback-item');
+        allFeedbacks.forEach(feedback => {
+
+            feedback.classList.remove("js--hidden");
+        });
+    })
+    select.addEventListener("change", () => {
+        $(".controls-section__button").removeClass("js--hidden")
+        var checkedOption = $(select).val();
+        filterFeedbacksByName(checkedOption)
+    });
+}
+
+function filterFeedbacksByRating() {
+    var allFeedbacks = document.querySelectorAll('.feedback-item');
+    var starRating = $("#slider").slider("values");
+
+    allFeedbacks.forEach(feedback => {
+        if (feedback.querySelector(".amount").textContent !=
+            $("#starRating").val()) {
+            feedback.classList.add("js--hidden");
+        } else {
+            feedback.classList.remove("js--hidden");
+        }
+    })
+
+}
+function filterFeedbacksByName(checkedValue) {
+    var allFeedbacks = document.querySelectorAll('.feedback-item');
+    allFeedbacks.forEach(feedback => {
+
+        if (feedback.querySelector(".feedback-item__caption").textContent != checkedValue) {
+            feedback.classList.add("js--hidden");
+        } else {
+            feedback.classList.remove("js--hidden");
+        }
+    })
+}
